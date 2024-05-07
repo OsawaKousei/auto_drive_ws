4de905a8732ee5cb36d0326927077f8a544d3451
OsawaKousei <jwithelis@gmail.com>
2024-05-07 20:52:08 +0900

    update setupsh

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,7 +7,7 @@ echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 
 # create workspace
 cd ~
-git https://github.com/OsawaKousei/auto_drive_ws.git
+git clone https://github.com/OsawaKousei/auto_drive_ws.git
 cd auto_drive_ws/auto_drive
 colcon build
 echo "source ~/auto_drive/install/setup.bash" >> ~/.bashrc
