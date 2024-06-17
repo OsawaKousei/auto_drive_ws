27caa3cea460151a91074969e714f28943f98e85
OsawaKousei <jwithelis@gmail.com>
2024-06-17 15:58:58 +0900

    create path publihser structuer

---
+++ auto_drive/src/local_path/include/local_path/path_publisher.hpp
@@ -0,0 +1,30 @@
+#ifndef LOCAL_PATH__PATH_PUBLISHER_HPP_
+#define LOCAL_PATH__PATH_PUBLISHER_HPP_
+
+#include "local_path/visibility_control.h"
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+namespace local_path {
+
+class PathPublisher : public rclcpp::Node {
+public:
+  TUTORIAL_PUBLIC
+  explicit PathPublisher(const rclcpp::NodeOptions &options);
+  virtual ~PathPublisher();
+
+private:
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
+
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+  nav_msgs::msg::Path path_;
+
+  std::mutex mutex_;
+};
+
+} // namespace local_path
+
+#endif // LOCAL_PATH__PATH_PUBLISHER_HPP_
