fd6b80f7820aada3a182f598a42d884df98e67c9
OsawaKousei <jwithelis@gmail.com>
2024-06-18 13:52:50 +0900

    archive simple pursuit

---
+++ auto_drive/src/path_pursuit/include/path_pursuit/simple_pursuit.hpp
@@ -0,0 +1,38 @@
+#ifndef PATH_PURSUIT__PATH_PURSUIT_HPP_
+#define PATH_PURSUIT__PATH_PURSUIT_HPP_
+
+#include "path_pursuit/visibility_control.h"
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+#include "pid.hpp"
+
+namespace path_pursuit {
+
+class PurePursuit : public rclcpp::Node {
+public:
+  TUTORIAL_PUBLIC
+  explicit PurePursuit(const rclcpp::NodeOptions &options);
+  virtual ~PurePursuit();
+
+private:
+  rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+
+  nav_msgs::msg::Path path_;
+  PID pid_x = PID(0.0, 0.0, 0.0);
+  PID pid_y = PID(0.0, 0.0, 0.0);
+
+  double kp, ki, kd;
+
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+  std::mutex mutex_;
+};
+
+} // namespace path_pursuit
+
+#endif // PATH_PURSUIT__PATH_PURSUIT_HPP_
---
+++ auto_drive/src/path_pursuit/src/simple_pursuit.cpp
@@ -0,0 +1,68 @@
+#include "path_pursuit/simple_pursuit.hpp"
+#include <rclcpp_components/register_node_macro.hpp>
+
+#include "pid.hpp"
+
+#include <fstream>
+
+using namespace std::chrono_literals;
+
+namespace path_pursuit {
+PurePursuit::PurePursuit(const rclcpp::NodeOptions &options)
+    : rclcpp::Node("pure_pursuit", options) {
+
+  // configure parameters
+  declare_parameter("kp", 0.0);
+  declare_parameter("ki", 0.0);
+  declare_parameter("kd", 0.0);
+  get_parameter("kp", kp);
+  get_parameter("ki", ki);
+  get_parameter("kd", kd);
+  std::cout << "kp: " << kp << " ki: " << ki << " kd: " << kd << std::endl;
+
+  pid_x = PID(kp, ki, kd);
+  pid_y = PID(kp, ki, kd);
+
+  path_sub_ = create_subscription<nav_msgs::msg::Path>(
+      "local_path", 1, [this](const nav_msgs::msg::Path::SharedPtr msg) {
+        path_ = *msg;
+      });
+
+  odom_sub_ = create_subscription<nav_msgs::msg::Odometry>(
+      "odom", 1, [this](const nav_msgs::msg::Odometry::SharedPtr msg) {
+        odom_callback(msg);
+      });
+  
+  cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 1);
+}
+
+PurePursuit::~PurePursuit() {}
+
+// TODO : syncronize path and odom
+
+void PurePursuit::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  // get current position
+  double x = msg->pose.pose.position.x;
+  double y = msg->pose.pose.position.y;
+
+  // check if path is empty
+  if (path_.poses.size() == 0) {
+    return;
+  }
+
+  // pid control
+  double target_x = path_.poses[5].pose.position.x;
+  double target_y = path_.poses[5].pose.position.y;
+  double cmd_x = pid_x.pid_ctrl(x, target_x);
+  double cmd_y = pid_y.pid_ctrl(y, target_y);
+
+  geometry_msgs::msg::Twist cmd_vel;
+  cmd_vel.linear.x = cmd_x;
+  cmd_vel.linear.y = cmd_y;
+
+  this->cmd_vel_pub_->publish(cmd_vel);
+}
+
+} // namespace path_pursuit
+
+RCLCPP_COMPONENTS_REGISTER_NODE(path_pursuit::PurePursuit)
