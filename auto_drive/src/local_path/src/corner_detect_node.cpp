1e3584abbb1dabcdf41fa431a9d447df3f36f6d5
OsawaKousei <jwithelis@gmail.com>
2024-06-18 21:35:06 +0900

    mov path2pc2 func to util

--- auto_drive/src/local_path/src/corner_detect_node.cpp
+++ auto_drive/src/local_path/src/corner_detect_node.cpp
@@ -11,6 +11,7 @@
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
 #include "local_path/path_publisher.hpp"
+#include "util/pcp.hpp"
 
 using namespace std::chrono_literals;
 namespace local_path {
@@ -36,7 +37,7 @@ private:
   void path_callback(const nav_msgs::msg::Path::SharedPtr path) {
     mutex_.lock();
 
-    auto corner_pc2 = path2pc2(*path);
+    auto corner_pc2 = pcp::path2pc2(*path);
     corner_pc2->header.frame_id = "map";
     corner_pc2->header.stamp = this->now();
     corner_publisher_->publish(*corner_pc2);
@@ -44,39 +45,6 @@ private:
     mutex_.unlock();
   }
 
-  sensor_msgs::msg::PointCloud2::SharedPtr path2pc2(nav_msgs::msg::Path path){
-    sensor_msgs::msg::PointCloud2 pc2;
-
-    pc2.header = path.header;
-    pc2.height = 1;
-    pc2.width = path.poses.size();
-    pc2.fields.resize(3);
-    pc2.fields[0].name = "x";
-    pc2.fields[0].offset = 0;
-    pc2.fields[0].datatype = sensor_msgs::msg::PointField::FLOAT32;
-    pc2.fields[0].count = 1;
-    pc2.fields[1].name = "y";
-    pc2.fields[1].offset = 4;
-    pc2.fields[1].datatype = sensor_msgs::msg::PointField::FLOAT32;
-    pc2.fields[1].count = 1;
-    pc2.fields[2].name = "z";
-    pc2.fields[2].offset = 8;
-    pc2.fields[2].datatype = sensor_msgs::msg::PointField::FLOAT32;
-    pc2.fields[2].count = 1;
-    pc2.is_bigendian = false;
-    pc2.point_step = 12;
-    pc2.row_step = 12 * path.poses.size();
-    pc2.is_dense = true;
-    pc2.data.resize(pc2.point_step * pc2.width);
-    float *data = reinterpret_cast<float *>(pc2.data.data());
-    for (int i = 0; i < path.poses.size(); i++) {
-      data[i * 3] = path.poses[i].pose.position.x;
-      data[i * 3 + 1] = path.poses[i].pose.position.y;
-      data[i * 3 + 2] = 0.0;
-    }
-
-    return std::make_shared<sensor_msgs::msg::PointCloud2>(pc2);
-  }
 };
   
 } // namespace local_path
